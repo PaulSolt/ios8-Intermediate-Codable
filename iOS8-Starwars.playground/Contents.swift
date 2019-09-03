0d4257a6fa1b05c0809e391b0d8fb6fa47f627bb
Paul Solt <PaulSolt@gmail.com>
2019-09-03 11:29:38 -0400

    Added basic web query

--- iOS8-Starwars.playground/Contents.swift
+++ iOS8-Starwars.playground/Contents.swift
@@ -1,2 +1,19 @@
 import UIKit
 
+// https://swapi.co/api/people/1/
+
+
+struct Person: Codable {
+    let name: String
+    let height: String  // Int vs. String
+    let hair_color: String // hairColor vs. hair_color
+}
+
+let url = URL(string: "https://swapi.co/api/people/1/")!
+let data = try! Data(contentsOf: url)
+
+let decoder = JSONDecoder()
+
+let luke = try! decoder.decode(Person.self, from: data)
+
+print(luke)
