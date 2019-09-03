ca1e2934834a06debd1863fef4d55102857572e2
Paul Solt <PaulSolt@gmail.com>
2019-09-03 11:57:23 -0400

    Parsing URLs for Films

--- iOS8-Starwars.playground/Contents.swift
+++ iOS8-Starwars.playground/Contents.swift
@@ -10,6 +10,10 @@ struct Person: Codable {
     let height: Int  // Int vs. String
     let hairColor: String // hairColor vs. hair_color
     
+    let films: [URL]
+    // let vehicles
+    // let starships
+    
     // Can name something else if you've overriden init()
     
 //    enum CodingKeys: String, CodingKey {
@@ -17,6 +21,7 @@ struct Person: Codable {
         case name
         case height // Set it equal to the value in JSON
         case hairColor = "hair_color"
+        case films
     }
     
     init(from decoder: Decoder) throws {
@@ -30,6 +35,16 @@ struct Person: Codable {
         
         hairColor = try container.decode(String.self, forKey: .hairColor)
         
+        var filmsContainer = try container.nestedUnkeyedContainer(forKey: .films)
+        
+        var filmUrls: [URL] = []
+        while filmsContainer.isAtEnd == false {
+            let filmString = try filmsContainer.decode(String.self)
+            if let url = URL(string: filmString) {
+                filmUrls.append(url)
+            }
+        }
+        films = filmUrls
     }
     
 }
